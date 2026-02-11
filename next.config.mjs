ede1b240591d3943a79eaf434ca23db2c963ac6c
root <root@burlywood-rosaline.play2go.cloud>
2026-02-11 23:08:18 +0000

    Fix basePath/assetPrefix for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/skytunnel-site",
+  assetPrefix: "/skytunnel-site",
   images: {
     unoptimized: true,
   },
